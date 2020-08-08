933e6a23bfdbbd8fb960ac42a67029539c609031
Yak Hng Ang <angyakhng8000@gmail.com>
2020-08-09 00:42:18 +0800

    complete query 4

--- queries.sql
+++ queries.sql
@@ -11,8 +11,10 @@ order by 'Sold' desc;
 -- query 2 - See which office performed the best during the december holiday season(cumulative)
 select T1.city, T1.country, count(*) 'Sales' from OrderDetailFacts ODF
 left join TimeDim TD on ODF.timeKey = TD.TimeKey
-left join (select employeeNumber, lastName, firstName, extension, email, E.officeCode, reportsTo, jobTitle, O.city, O.country from Employees E
-left join Offices O on E.officeCode = O.officeCode) T1 on ODF.employeeNumber = T1.employeeNumber
+left join (
+    select employeeNumber, lastName, firstName, extension, email, E.officeCode, reportsTo, jobTitle, O.city, O.country from Employees E
+    left join Offices O on E.officeCode = O.officeCode
+) T1 on ODF.employeeNumber = T1.employeeNumber
 where TD.Month = 12
 group by T1.city, T1.country
 order by Sales desc;
@@ -21,34 +23,21 @@ order by Sales desc;
 select T1.firstName, T1.lastName, T1.jobTitle, P.productName, count(P.productCode) 'Sold' from OrderDetailFacts ODF
 left join Employees E on ODF.employeeNumber = E.employeeNumber
 left join Products P on ODF.productCode = P.productCode
-left join (select top(1) E.employeeNumber, E.firstName, E.lastName, E.jobTitle, count(*) 'Sales' from OrderDetailFacts ODF
-left join Employees E on ODF.employeeNumber = E.employeeNumber
-group by E.employeeNumber, E.firstName, E.lastName, E.jobTitle
-order by Sales desc) T1 on ODF.employeeNumber = T1.employeeNumber
+left join (
+    select top(1) E.employeeNumber, E.firstName, E.lastName, E.jobTitle, count(*) 'Sales' from OrderDetailFacts ODF
+    left join Employees E on ODF.employeeNumber = E.employeeNumber
+    group by E.employeeNumber, E.firstName, E.lastName, E.jobTitle
+    order by Sales desc
+) T1 on ODF.employeeNumber = T1.employeeNumber
 where ODF.employeeNumber = T1.employeeNumber
 group by T1.firstName, T1.lastName, T1.jobTitle, P.productName
 order by Sold desc;
 
--- query 4 - percent of orders coming from different countries (pain/suffering)
-select count(T1.orderNumber) 'totalOrders', T1.country from OrderDetailFacts ODF
-left join Orders O on ODF.orderNumber = O.orderNumber
-left join Customers C on ODF.customerNumber = C.customerNumber
-where ODF.orderNumber = T1.orderNumber
-group by T1.country
-order by totalOrders desc;
-
--- query 4.1 - amount of money made from each customer (hope?)
-select P.amount, C.customerNumber from OrderDetailFacts ODF
-left join Customers C on C.customerNumber = ODF.customerNumber
-left join Payments P on C.customerNumber = P.customerNumber
-
--- select C.customerNumber, 
--- left join Payments P on C.customerNumber = 
-
-
--- SELECT  *
--- FROM    (SELECT ID, SKU, Product,
---                 ROW_NUMBER() OVER (PARTITION BY PRODUCT ORDER BY ID) AS RowNumber
---          FROM   MyTable
---          WHERE  SKU LIKE 'FOO%') AS a
--- WHERE   a.RowNumber = 1
+-- query 4 - find out how much profit each product made --
+select T1.productName, sum(T1.[Profit Made]) 'Total Profit' from (
+    select P.productName, (count(*) * ODF.priceEach) - (count(*) * P.buyPrice) 'Profit Made' from OrderDetailFacts ODF
+    left join Products P on P.productCode = ODF.productCode
+    group by P.productName, ODF.priceEach, P.buyPrice
+) T1
+group by T1.productName
+order by [Total Profit] desc;
